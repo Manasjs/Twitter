ab9752f60208a39705e8d6e1707a2eb1c187ca84
ManasPW <manaskumarshaw9@gmail.com>
2025-07-29 13:49:07 +0530

    Added controller and routes for twitter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import {connect} from './config/database.js';
 
-import TweetService from './services/tweet-service.js';
+import apiRoutes from './routes/index.js';
+
+
 const app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api',apiRoutes);
 
 app.listen(3000,async()=>{
     console.log("server started")
     await connect ();
     console.log("mongo db connected");
-    const ser = new TweetService();
-    const tweet= await ser.create({content:'Watching #movies is #awasome'});
-    console.log(tweet);
-      
+    
 });
