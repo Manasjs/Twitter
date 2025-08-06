13a496673026c7196b311fa74eaa897f756240fe
ManasPW <manaskumarshaw9@gmail.com>
2025-08-06 13:20:14 +0530

    Added passport logic

--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -24,3 +24,22 @@ export const signup = async(req,res)=>{
         }); 
    }
 }
+
+export const login = async(req,res)=>{
+      try {
+        const token = await userService.signin(req.body);
+          return res.status(200).json({
+             success:true,
+             message:'successfully logged in',
+             data:token,
+             err:{}
+        })
+      } catch (error) {
+        return res.status(500).json({
+             success:false,
+             message:'somthing went wrong',
+             data:{},
+             err:error
+        });
+      }
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+import { passportAuth } from './config/jwt-middleware.js';
 
 import {connect} from './config/database.js';
 
 import apiRoutes from './routes/index.js';
-import {TweetRepository, UserRepository} from  './repository/index.js';
-import LikeService from './services/like-service.js';
-
 
 const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize()) //initializing passport object
+passportAuth(passport);
+
 
 app.use('/api',apiRoutes);
 
 app.listen(3000,async()=>{
     console.log("server started")
     await connect ();
-    console.log("mongo db connected");
-
-    // const userRepo = new UserRepository();
-    // const tweetRepo = new TweetRepository();
-    // const tweets = await tweetRepo.getAll(0,10);
-
-    
-    // let user = await userRepo.create({
-    //    email:'newuser@example.com',
-    //    password:'123456',
-    //    name:'new user'
-    // });
-    
-    // const likeService = new LikeService();
-    // await likeService.toggleLike(tweets[0],'Tweet',user.id);
+    console.log("mongo db connected")
 
     
 });
